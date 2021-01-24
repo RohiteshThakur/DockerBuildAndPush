7801a5081b3d8de81de009b0fef69e595620e770
RoT <rohiteshthakur@gmail.com>
2021-01-24 20:33:07 +0000

    first commit

---
+++ GoCode/src/hellogo/hellogo.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from Golang!"))
+	})
+	http.ListenAndServe(":8000", nil)
+}
